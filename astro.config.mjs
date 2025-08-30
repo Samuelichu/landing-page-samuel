8809b720090b13603a267140fba58f4555c51dc3
Samuelichu <chavezzsam@gmail.com>
2025-08-30 12:32:56 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
-export default defineConfig({integrations: [tailwind()],});
+export default defineConfig({
+    integrations: [tailwind()],
+    site: 'https://github.com/Samuelichu',
+    base: 'landing-page-samuel',
+});
